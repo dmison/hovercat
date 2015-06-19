fc56b6c08e17a19c0cf932df51ec53a2bf196897
Darrin Mison <dmison@me.com>
2015-06-19 20:14:34 +1000

    some bootstrap layout

--- components/ErrorConsole.jsx
+++ components/ErrorConsole.jsx
@@ -4,9 +4,9 @@
   var ErrorConsole = React.createClass({
 
     render: function(){
-      var yamlDiv = (this.props.yaml === '') ? '' : <div>YAML: {this.props.yaml}</div>
-      var textDiv = (this.props.text === '') ? '' : <div>TEXT: {this.props.text}</div>
-      var htmlDiv = (this.props.html === '') ? '' : <div>HTML: {this.props.html}</div>
+      var yamlDiv = (this.props.yaml === '') ? '' : <div><span className='label label-danger'>YAML</span> {this.props.yaml}</div>
+    var textDiv = (this.props.text === '') ? '' : <div><span className='label label-danger'>TEXT</span> {this.props.text}</div>
+  var htmlDiv = (this.props.html === '') ? '' : <div><span className='label label-danger'>HTML</span> {this.props.html}</div>
 
       return (
 
--- components/HovercraftApp.jsx
+++ components/HovercraftApp.jsx
@@ -1,91 +1,105 @@
-(function(){
-var React = require('react');
+(function () {
+  var React = require('react');
 
-var Editor = require('./Editor.jsx');
-var ErrorConsole = require('./ErrorConsole.jsx');
-var TextPreviewer = require('./Previewer.jsx');
-var TabbedArea = require('react-bootstrap/lib/TabbedArea');
-var TabPane = require('react-bootstrap/lib/TabPane');
+  var Editor = require('./Editor.jsx');
+  var ErrorConsole = require('./ErrorConsole.jsx');
+  var TextPreviewer = require('./Previewer.jsx');
+  var TabbedArea = require('react-bootstrap/lib/TabbedArea');
+  var TabPane = require('react-bootstrap/lib/TabPane');
+  var MainMenu = require('./MainMenu.jsx');
 
-var HovercraftApp = React.createClass({
+  var HovercraftApp = React.createClass({
 
-  getInitialState: function(){
-    return {
-      content: '',
-      htmlTemplate: '',
-      textTemplate: '',
-      htmlOutput: '',
-      textOutput: '',
-      errorsYAML: '',
-      errorsTEXT: '',
-      errorsHTML: ''
+    getInitialState: function () {
+      return {
+        content: '',
+        htmlTemplate: '',
+        textTemplate: '',
+        htmlOutput: '',
+        textOutput: '',
+        errorsYAML: '',
+        errorsTEXT: '',
+        errorsHTML: ''
+      }
+    },
+
+    contentUpdated: function (text) {
+      this.setState({
+        content: text
+      });
+
+    },
+
+    receiveNewErrors: function (type, errorText) {
+
+      if (type === 'yaml') {
+        this.setState({
+          errorsYAML: errorText
+        });
+      }
+      if (type === 'text') {
+        this.setState({
+          errorsTEXT: errorText
+        });
+      }
+      if (type === 'html') {
+        this.setState({
+          errorsHTML: errorText
+        });
+      }
+    },
+
+    textTemplateUpdated: function (text) {
+      this.setState({
+        textTemplate: text
+      });
+    },
+    htmlTemplateUpdated: function (text) {
+      this.setState({
+        htmlTemplate: text
+      });
+    },
+
+    render: function () {
+      var textOut = this.state.textOutput;
+      var errors = this.state.errors;
+
+      return (
+        <div>
+          <MainMenu />
+          <div className="row">
+            <div className="col-sm-6">
+              <TabbedArea defaultActiveKey={1}>
+                <TabPane eventKey={1} tab='Content'>
+                  <Editor content={this.state.content} mode='yaml' onChange={this.contentUpdated} theme='tomorrow'/>
+                </TabPane>
+                <TabPane eventKey={2} tab='Text Template'>
+                  <Editor content={this.state.textTemplate} mode='markdown' onChange={this.textTemplateUpdated} theme='tomorrow'/>
+                </TabPane>
+                <TabPane eventKey={3} tab='HTML Template'>
+                  <Editor content={this.state.htmlTemplate} mode='html' onChange={this.htmlTemplateUpdated} theme='tomorrow'/>
+                </TabPane>
+              </TabbedArea>
+              <ErrorConsole html={this.state.errorsHTML} text={this.state.errorsTEXT} yaml={this.state.errorsYAML}/>
+            </div>
+            <div className="col-sm-6">
+              <TabbedArea defaultActiveKey={1}>
+                <TabPane eventKey={1} tab='Text Preview'>
+                  <TextPreviewer content={this.state.content} returnError={this.receiveNewErrors} template={this.state.textTemplate} type='text'/>
+                </TabPane>
+                <TabPane eventKey={2} tab='HTML Preview'>
+                  <TextPreviewer content={this.state.content} returnError={this.receiveNewErrors} template={this.state.htmlTemplate} type='html'/>
+                </TabPane>
+              </TabbedArea>
+            </div>
+          </div>
+        </div>
+      )
     }
-  },
 
-  contentUpdated: function(text){
-    this.setState({ content: text });
+  });
 
-  },
-
-  receiveNewErrors: function(type, errorText){
-
-    if (type === 'yaml'){
-      this.setState({errorsYAML: errorText});
-    }
-    if (type === 'text'){
-      this.setState({errorsTEXT: errorText});
-    }
-    if (type === 'html'){
-      this.setState({errorsHTML: errorText});
-    }
-  },
-
-  textTemplateUpdated: function(text){
-    this.setState({ textTemplate: text });
-  },
-  htmlTemplateUpdated: function(text){
-    this.setState({ htmlTemplate: text});
-  },
-
-  render: function(){
-    var textOut = this.state.textOutput;
-    var errors = this.state.errors;
-
-    return (
-      <div>
-        <h2>Hovercraft</h2>
-
-          <TabbedArea defaultActiveKey={1}>
-            <TabPane eventKey={1} tab='Content'>
-              <Editor mode='yaml' theme='tomorrow' content={this.state.content} onChange={this.contentUpdated} />
-            </TabPane>
-            <TabPane eventKey={2} tab='Text Template'>
-              <Editor mode='markdown' theme='tomorrow' content={this.state.textTemplate} onChange={this.textTemplateUpdated} />
-            </TabPane>
-            <TabPane eventKey={3} tab='HTML Template'>
-              <Editor mode='html' theme='tomorrow' content={this.state.htmlTemplate} onChange={this.htmlTemplateUpdated} />
-            </TabPane>
-          </TabbedArea>
-
-          <ErrorConsole text={this.state.errorsTEXT} yaml={this.state.errorsYAML} html={this.state.errorsHTML} />
-
-          <TabbedArea defaultActiveKey={1}>
-            <TabPane eventKey={1} tab='Text Preview'>
-              <TextPreviewer content={this.state.content} type='text' template={this.state.textTemplate} returnError={this.receiveNewErrors}/>
-            </TabPane>
-            <TabPane eventKey={2} tab='HTML Preview'>
-              <TextPreviewer content={this.state.content} type='html' template={this.state.htmlTemplate} returnError={this.receiveNewErrors}/>
-            </TabPane>
-          </TabbedArea>
-
-
-      </div>
-    )
-  }
-
-});
-
-module.exports = HovercraftApp;
+  module.exports = HovercraftApp;
 
 })();
 
