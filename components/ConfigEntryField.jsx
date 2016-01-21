2aace6bfc44a32e984865cf7a9ef2fdf5d5eb15b
Darrin Mison <dmison@me.com>
2016-01-21 16:09:17 +1000

    more differences between text and boolean

--- components/ConfigEntryField.jsx
+++ components/ConfigEntryField.jsx
@@ -7,30 +7,42 @@
       var element;
 
       if(this.props.type === 'text'){
-        element = <input className='form-control' onChange={this.valueChanged} value={this.props.configValue}/>;
+        element = (
+          <div className='form-group'>
+            <label className='col-sm-3 control-label'>{this.props.label}</label>
+            <div className='col-sm-4'>
+              <input className='form-control' onChange={this.valueChanged} value={this.props.configValue}/>
+            </div>
+            <div className='col-sm-5 helptext'>
+              <p>
+                {this.props.description}
+              </p>
+            </div>
+          </div>
+
+        );
       }
 
       if(this.props.type === 'boolean'){
-        element = <input
-                    type='checkbox'
-                    label={this.props.label}
-                    checked={this.props.configValue}
-                    onChange={this.valueChanged} />;
+        element = (
+          <div className='form-group'>
+            <label className='col-sm-4 col-sm-offset-3 control-label pull-left'>
+                <input  type='checkbox' className='pull-left'
+                        label={this.props.label}
+                        checked={this.props.configValue}
+                        onChange={this.valueChanged} /> <span className='pull-left' style={{marginLeft: 5}}>{this.props.label}</span>
+              </label>
+            <div className='col-sm-5 helptext'>
+              <p>
+                {this.props.description}
+              </p>
+            </div>
+          </div>
+
+        );
       }
 
-      return (
-        <div className='form-group'>
-          <label className='col-sm-2 control-label'>{this.props.label}</label>
-          <div className='col-sm-4'>
-            {element}
-          </div>
-          <div className='col-sm-6 helptext'>
-          <p>
-            {this.props.description}
-          </p>
-        </div>
-        </div>
-      );
+      return element;
     },
 
     valueChanged: function(event) {
@@ -47,19 +59,3 @@
   module.exports = ConfigEntryField;
 
 })();
-
-// <div className="form-group" id="gitRepo_grp">
-//   <label for="gitRepo" className="col-sm-2 control-label">Github Repo</label>
-//   <div className="col-sm-4">
-//     <input type="text" id="gitRepo" className="form-control">
-//   </div>
-//   <div className="col-sm-6 helptext">
-//     The name of the repository with your Jekyll site source
-//   </div>
-// </div>
-
-
-// <Input  type='checkbox'
-//         label='HTML'
-//         checked={this.state.includeHTML}
-//         onChange={this.checkIncludeHTML} />
