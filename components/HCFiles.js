97c4293a3183a4900f211f2820a16e26f703289c
Darrin Mison <dmison@me.com>
2016-01-21 11:36:06 +1000

    show alerts if config file fails to load or parse

--- components/HCFiles.js
+++ components/HCFiles.js
@@ -24,7 +24,7 @@
           dataInput = JSON.parse(data);
         } catch (e) {
           dataInput = {
-              error: e.message
+            error: e.message
           };
         }
 
@@ -53,7 +53,7 @@
   };
 
   var writeConfigFile = function(config, configfilepath, done){
-    console.log("writing");
+
     // turn config into YAML
     var output = YAML.stringify(config, 4);
     var options = { flag: 'w' };
@@ -64,7 +64,6 @@
     fs.access(configDir, fs.F_OK, function(err){
       //no: mkdir
       if(err){
-        console.log('making dir: %s', configDir)
         fs.mkdir(configDir);
       }
       //yes: can it be written to?
@@ -83,11 +82,11 @@
 
   };
 
-  // if ~/.hovercat exists then read & return JSON, otherwise create
+  // if config file exists then read & return JSON, otherwise create
   // done(config json)
   var readConfigFile = function(defaultConfig, configfilepath, done){
     if (configfilepath === ''){
-      console.log('no filename specified for config');
+      console.log('no filename specified for config yet');
       return;
     }
     fs.access(configfilepath, fs.F_OK | fs.W_OK | fs.R_OK, function(error){
@@ -99,17 +98,17 @@
         fs.readFile(configfilepath, 'utf-8', function(err, data) {
 
           if (err) {
-            console.log("failed to read config\n%s", err)
+            alert('failed to read config\n%s', err);
           } else {
             var loadedConfig = {};
             // parse YAML to JSON
             try {
               loadedConfig = YAML.parse(data);
             } catch(e){
-              console.log("yaml parse error: %s", e)
-              loadedConfig = {}
+              alert('yaml parse error: %s', e);
+              loadedConfig = {};
             }
-            done(loadedConfig)
+            done(loadedConfig);
           }
         });
       }
