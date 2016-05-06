e40920c484989c1ebff654cd2ed9258fecfb6c17
Darrin Mison <dmison@me.com>
2016-05-06 17:30:26 +1000

    added uistate to store

--- src/Store/index.js
+++ src/Store/index.js
@@ -1,18 +1,22 @@
-const content_reducers = require('../Content/reducers.js');
-const error_reducers = require('../Errors/reducers.js');
-const template_reducers = require('../Template/reducers.js');
+const {content_reducer} = require('../Content/reducers.js');
+const {error_reducer} = require('../Errors/reducers.js');
+const {template_reducer} = require('../Template/reducers.js');
+const {uistate_reducer} = require('../UIState/reducers.js');
+
 const Redux = require('redux');
 
 const AppReducer = Redux.combineReducers({
-  content: content_reducers.content_reducer,
-  errors: error_reducers.error_reducer,
-  templates: template_reducers.template_reducer
+  content: content_reducer,
+  errors: error_reducer,
+  templates: template_reducer,
+  uistate: uistate_reducer
 });
 
 const defaultState = {
   templates: [],
   content: '',
-  errors: []
+  errors: [],
+  uistate: { 'saved': true, 'wrap': false, 'resourcesPath': '', 'filename': 'untitled' }
 };
 
 const store = Redux.createStore(AppReducer, defaultState);
