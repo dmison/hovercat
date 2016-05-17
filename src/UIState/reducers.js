fba4617c64622b3b654c5e66fd470e6e1530929f
D Mison <dmison@redhat.com>
2016-05-17 13:42:44 +1000

    UIstate reducer was not pure

--- src/UIState/reducers.js
+++ src/UIState/reducers.js
@@ -2,17 +2,17 @@ const uistate_reducer = (state = {}, action) => {
 
   switch(action.type){
   case 'SET_SAVED':
-    return Object.assign( state, { saved: action.saved } );
+    return Object.assign( {}, state, { saved: action.saved } );
   case 'SET_SAVING':
-    return Object.assign( state, { saving: action.saving } );
+    return Object.assign( {}, state, { saving: action.saving } );
   case 'SET_WRAP':
-    return Object.assign( state, { wrap: action.enabled } );
+    return Object.assign( {}, state, { wrap: action.enabled } );
   case 'SET_FILENAME':
-    return Object.assign( state, { filename: action.filename } );
+    return Object.assign( {}, state, { filename: action.filename } );
   case 'SET_RESOURCES_PATH':
-    return Object.assign( state, { resourcesPath: action.path } );
+    return Object.assign( {}, state, { resourcesPath: action.path } );
   case 'SET_ACTIVE':
-    return Object.assign( state, { active: action.active } );
+    return Object.assign( {}, state, { active: action.active } );
   default:
     return state;
   }
