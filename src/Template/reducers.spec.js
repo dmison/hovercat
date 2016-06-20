696289f3f480819e4fbbb9bf1713b1156c1e33e3
D Mison <dmison@redhat.com>
2016-06-20 16:31:43 +1000

    type strings didn't match actual UI

--- src/Template/reducers.spec.js
+++ src/Template/reducers.spec.js
@@ -12,7 +12,7 @@ describe('testing template reducers', function(){
   it('add one template', function(){
     const content = 'here is some arbitrary {{content}}.';
     const name = 'basic text';
-    const type = 'TEXT';
+    const type = 'markdown';
 
     const actualNewState = template_reducer(undefined, addNewTemplate(name, type, content));
 
@@ -26,11 +26,11 @@ describe('testing template reducers', function(){
 
     const templateOne = 'here is some arbitrary {{content}}.';
     const nameOne = 'text email';
-    const typeOne = 'TEXT';
+    const typeOne = 'markdown';
 
     const templateTwo = 'here is some more arbitrary {{content}}.';
     const nameTwo = 'html email';
-    const typeTwo = 'HTML';
+    const typeTwo = 'html';
 
     let actualNewTemplates = [];
     actualNewTemplates = template_reducer(actualNewTemplates, addNewTemplate(nameOne, typeOne, templateOne));
@@ -63,7 +63,7 @@ describe('testing template reducers', function(){
     const id = '234-235-235-235-235-222';
     const template = 'here is some arbitrary {{content}}.';
     const name = 'basic text';
-    const type = 'TEXT';
+    const type = 'markdown';
 
     const startingTemplates = [
       {
@@ -89,7 +89,7 @@ describe('testing template reducers', function(){
     const id = '234-235-235-235-235-222';
     const template = 'here is some <b>arbitrary</b> {{content}}.';
     const name = 'basic text';
-    const type = 'TEXT';
+    const type = 'markdown';
 
     const startingTemplates = [
       {
@@ -100,7 +100,7 @@ describe('testing template reducers', function(){
       }
     ];
 
-    const newType = 'HTML';
+    const newType = 'html';
 
     const actualUpdatedTemplates = template_reducer(startingTemplates, updateTemplate(id, name, newType));
     expect(actualUpdatedTemplates[0].content).to.equal(template);
@@ -116,13 +116,13 @@ describe('testing template reducers', function(){
       id: '234-235-235-235-235-222',
       content: 'here is some arbitrary {{content}}.',
       name: 'template two',
-      type: 'TEXT'
+      type: 'markdown'
     };
     const templateTwo = {
       id: '493-235-746-235-255',
       content: 'here is some other arbitrary {{content}}.',
       name: 'template one',
-      type: 'TEXT'
+      type: 'markdown'
     };
 
     const startingTemplates = [ templateOne, templateTwo ];
@@ -138,13 +138,13 @@ describe('testing template reducers', function(){
       id: '234-235-235-235-235-222',
       content: 'here is some arbitrary {{content}}.',
       name: 'template two',
-      type: 'TEXT'
+      type: 'markdown',
     };
     const templateTwo = {
       id: '493-235-746-235-255',
       content: 'here is some other arbitrary {{content}}.',
       name: 'template one',
-      type: 'TEXT'
+      type: 'markdown',
     };
 
     const templatesToImport = [ templateOne, templateTwo ];
@@ -160,12 +160,12 @@ describe('testing template reducers', function(){
       id: '234-235-235-235-235-222',
       content: 'here is some arbitrary {{content}}.',
       name: 'template two',
-      type: 'TEXT'
+      type: 'markdown'
     };
     const templateTwo = {
       content: 'here is some other arbitrary {{content}}.',
       name: 'template one',
-      type: 'TEXT'
+      type: 'markdown'
     };
 
     const templatesToImport = [ templateTwo ];
