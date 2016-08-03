ce975c95d120dc0159c1f9ba22f2a7a34a7173e4
D Mison <dmison@me.com>
2016-08-03 13:34:37 +1000

    added email actions

--- src/Config/actions.js
+++ src/Config/actions.js
@@ -1,14 +1,14 @@
-const setEditorWrap = (enabled) => {
-  return {
-    type: 'SET_WRAP',
-    enabled: enabled
-  };
-};
-
 const importConfig = (config) => {
   return {
     type: 'IMPORT_CONFIG',
     config: config
+  };
+};
+
+const setEditorWrap = (enabled) => {
+  return {
+    type: 'SET_WRAP',
+    enabled: enabled
   };
 };
 
@@ -19,9 +19,56 @@ const setBitlyToken = (token) => {
   };
 };
 
+const setGmailUsername = (username) => {
+  return {
+    type: 'SET_GMAIL_USERNAME',
+    username: username
+  };
+};
+
+const setGmailPassword = (appPassword) => {
+  return {
+    type: 'SET_GMAIL_APP_PASSWORD',
+    appPassword: appPassword
+  };
+};
+
+const setSMTPSender = (sender) => {
+  return {
+    type: 'SET_SMTP_SENDER',
+    sender: sender
+  };
+};
+
+const setSMTPHost = (host) => {
+  return {
+    type: 'SET_SMTP_HOST',
+    host: host
+  };
+};
+
+const setSMTPPort = (port) => {
+  return {
+    type: 'SET_SMTP_PORT',
+    port: port
+  };
+};
+
+const setSMTPTLSRejectUnauthorized = (enabled) => {
+  return {
+    type: 'SET_SMTP_TLS_REJECT_UNAUTHORIZED',
+    enabled: enabled
+  };
+};
 
 module.exports = {
-  setEditorWrap: setEditorWrap,
   importConfig: importConfig,
-  setBitlyToken: setBitlyToken
+  setEditorWrap: setEditorWrap,
+  setBitlyToken: setBitlyToken,
+  setGmailUsername: setGmailUsername,
+  setGmailPassword: setGmailPassword,
+  setSMTPSender: setSMTPSender,
+  setSMTPHost: setSMTPHost,
+  setSMTPPort: setSMTPPort,
+  setSMTPTLSRejectUnauthorized: setSMTPTLSRejectUnauthorized
 };
