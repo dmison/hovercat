5c7dc25dcec5fa429fdb205ceb081515d7b45512
D Mison <dmison@me.com>
2016-08-05 15:54:34 +1000

    now using default state exported from config reducer in tests

--- src/Config/reducers.spec.js
+++ src/Config/reducers.spec.js
@@ -1,29 +1,42 @@
 /* global describe it */
 const expect = require('chai').expect;
-const {config_reducer} = require('./reducers.js');
+const {default_config, config_reducer} = require('./reducers.js');
 const {
   setEditorWrap,
   importConfig,
-  setBitlyToken
+  setBitlyToken,
+  setGmailUsername
 } = require('./actions.js');
 
-describe('testing config reducers', () => {
+describe('testing config reducers', function(){
 
-  it('set editor wrap to true', ()=>{
-    const startingState = {};
-    const expectedEndState = { 'editor': {'wrapEnabled': true}};
+  it('should change the editor wrap value to true', function(){
+    const startingState = default_config;
+    let expectedEndState = default_config;
+    expectedEndState.editor.wrapEnabled = true;
     const actualNewState = config_reducer(startingState, setEditorWrap(true));
     expect(actualNewState).to.deep.equal(expectedEndState);
   });
 
-  it('set editor wrap from true to false', ()=>{
-    const startingState = { 'editor': {'wrapEnabled': true}};
-    const expectedEndState = { 'editor': {'wrapEnabled': false}};
+  it('should change the editor wrap from true to false', function(){
+    const startingState = default_config;
+    let expectedEndState = default_config;
+    expectedEndState.editor.wrapEnabled = false;
     const actualNewState = config_reducer(startingState, setEditorWrap(false));
     expect(actualNewState).to.deep.equal(expectedEndState);
   });
 
-  it('dump config from file', ()=>{
+  it('should update gmail username without changing other values', function(){
+    const startingState = default_config;
+    let expectedEndState = default_config;
+    const newUsername = 'new@username.com';
+    expectedEndState.email.gmail.username = newUsername;
+    const actualNewState = config_reducer(startingState, setGmailUsername(newUsername));
+    expect(actualNewState).to.deep.equal(expectedEndState);
+  });
+
+
+  it('dump config from file', function(){
     const incomingConfig = {
       'editor': {
         'wrapEnabled': true
@@ -44,94 +57,44 @@ describe('testing config reducers', () => {
         }
       }
     };
-    const actual = config_reducer({}, importConfig(incomingConfig));
-    expect(actual).to.deep.equal(incomingConfig);
+
+    let expectedState = default_config;
+    expectedState.editor.wrapEnabled = true;
+    expectedState.bitlyAccessToken = 'randomtokencharacters1234';
+    expectedState.email.gmail.username = 'some@email.com';
+    expectedState.email.gmail.appPassword = 'passwordhere';
+    expectedState.email.smtp.host = 'mail.company.com';
+    expectedState.email.smtp.port = 25;
+    expectedState.email.smtp.tls.rejectUnauthorized = false;
+
+    const actualState = config_reducer(undefined, importConfig(incomingConfig));
+    expect(actualState).to.deep.equal(expectedState);
   });
 
-  it('dump config from file into partial config', ()=>{
+
+  it('dump partial config from file', function(){
     const incomingConfig = {
       'editor': {
         'wrapEnabled': true
       },
-      'bitlyAccessToken': 'randomtokencharacters1234',
-      'email':{
-        'defaultSender': 'some@email.com',
-        'gmail':{
-          'username': 'some@email.com',
-          'appPassword': 'passwordhere'
-        },
-        'smtp':{
-          'host': 'mail.company.com',
-          'port': 25,
-          'tls': {
-            'rejectUnauthorized': false
-          }
-        }
-      }
-    };
-    const startingState = {
-      'editor': {
-        'wrapEnabled': false
-      },
       'bitlyAccessToken': 'randomtokencharacters1234'
     };
 
-    const actual = config_reducer(startingState, importConfig(incomingConfig));
-    expect(actual).to.deep.equal(incomingConfig);
+    const expectedState = default_config;
+    expectedState.editor.wrapEnabled = true;
+    expectedState.bitlyAccessToken = 'randomtokencharacters1234';
+
+    const actualState = config_reducer(default_config, importConfig(incomingConfig));
+
+    expect(actualState).to.deep.equal(expectedState);
   });
 
-  it('dump config from file with merge', ()=>{
-    const incoming = {
-      'editor': {
-        'wrapEnabled': true
-      },
-      'bitlyAccessToken': 'randomtokencharacters1234'
-    };
-    const starting = {
-      'email':{
-        'defaultSender': 'some@email.com',
-        'gmail':{
-          'username': 'some@email.com',
-          'appPassword': 'passwordhere'
-        },
-        'smtp':{
-          'host': 'mail.company.com',
-          'port': 25,
-          'tls': {
-            'rejectUnauthorized': false
-          }
-        }
-      }
-    };
-    const expected = {
-      'editor': {
-        'wrapEnabled': true
-      },
-      'bitlyAccessToken': 'randomtokencharacters1234',
-      'email':{
-        'defaultSender': 'some@email.com',
-        'gmail':{
-          'username': 'some@email.com',
-          'appPassword': 'passwordhere'
-        },
-        'smtp':{
-          'host': 'mail.company.com',
-          'port': 25,
-          'tls': {
-            'rejectUnauthorized': false
-          }
-        }
-      }
-    };
-
-    const actual = config_reducer(starting, importConfig(incoming));
-    expect(actual).to.deep.equal(expected);
-  });
-
-  it('saving bitly access token', ()=>{
+  it('saving bitly access token', function(){
     const tokenCode = 'randomtokencharacters8453';
-    const expectedEndState = { 'bitlyAccessToken': tokenCode};
-    const actualNewState = config_reducer({}, setBitlyToken(tokenCode));
+    const startingState = default_config;
+    let expectedEndState = default_config;
+    expectedEndState.bitlyAccessToken = tokenCode;
+    const actualNewState = config_reducer(startingState, setBitlyToken(tokenCode));
     expect(actualNewState).to.deep.equal(expectedEndState);
   });
 
