b10f50f37504b5c9ddc8d81fc2926c5a1733b74a
D Mison <dmison@me.com>
2016-09-02 14:08:52 +1000

    updated SendEmail component with new features

--- src/Email/SendEmail.jsx
+++ src/Email/SendEmail.jsx
@@ -1,6 +1,9 @@
 const React = require('react');
 const {hashHistory} = require('react-router');
+const EmailEntryItem = require('./EmailEntryItem.jsx');
+const EmailFormatSelector = require('./EmailFormatSelector.jsx');
 const {send} = require('./EmailUtils.js');
+const {inlineCSS} = require('../Compiler');
 
 class SendEmail extends React.Component {
 
@@ -13,11 +16,16 @@ class SendEmail extends React.Component {
       from: '',
       replyTo: '',
       subject: '',
-      html: true,
-      text: true,
-      htmlSource: '',
-      textSource: ''
+      html: false,
+      text: false,
+      htmlSelection: '',
+      textSelection: '',
+      sending: false
     };
+    this._sendEmail = this._sendEmail.bind(this);
+    this._getContentForName = this._getContentForName.bind(this);
+    this._getOutputForType = this._getOutputForType.bind(this);
+    this._okToSend = this._okToSend.bind(this);
   }
 
   // on initial mount
@@ -27,10 +35,70 @@ class SendEmail extends React.Component {
       from: this.props.config.gmail.username,
       replyTo: this.props.config.gmail.username
     });
+
+    //if there are no initial selections, select the first of each
+    if(this._getOutputForType('html').length > 0 && this.state.htmlSelection === ''){
+      this.setState({htmlSelection: this._getOutputForType('html')[0].name});
+    }
+    if(this._getOutputForType('markdown').length > 0 && this.state.textSelection === ''){
+      this.setState({textSelection: this._getOutputForType('markdown')[0].name});
+    }
+
+  }
+
+  _sendEmail(){
+    this.setState({ sending: true });
+    const htmlContent = inlineCSS(this._getContentForName(this.state.htmlSelection));
+
+    send(
+      this.props.config,
+      (this.state.useGmail? 'gmail':'smtp'),
+      this.state.sendTo,
+      this.state.subject,
+      this.state.html? htmlContent:'',
+      this.state.text? this._getContentForName(this.state.textSelection):'',
+      this.state.from,
+      this.state.replyTo,
+      (error)=>{
+        // todo: component *might* be unmounted before this happens: is there a nice what to avoid the logged error?
+        this.setState({ sending: false });
+        setTimeout(()=>{
+          if(error){
+            alert('Error sending email:', error);
+          } else {
+            alert('Email successfully sent.');
+          }
+
+        }, 10);
+      }
+    );
+
+  }
+
+
+  _gmailConfigOK(){
+
+  }
+
+  _smtpConfigOK(){
+
+  }
+
+  _okToSend(){
+
+  }
+
+  _getContentForName(name){
+    return this.props.outputs.find((output)=>{
+      return output.name === name;
+    }).output;
+  }
+
+  _getOutputForType(type){
+    return this.props.outputs.filter((output)=>{ return output.type === type; });
   }
 
   render(){
-
     const style = {
       height: this.props.height,
       marginTop: 12,
@@ -47,22 +115,36 @@ class SendEmail extends React.Component {
             <div className='panel-heading'>
               <h3 className='panel-title pull-left'>Send Email</h3>
               <button className='btn btn-default pull-right' onClick={()=>{hashHistory.push('/');}} >Close</button>
-              <button className='btn btn-info pull-right' style={{marginRight: 10}} >Send</button>
+              <button disabled={this.state.sending} className='btn btn-info pull-right' style={{marginRight: 10}} onClick={this._sendEmail}>
+                {this.state.sending?<span className='pull-right sendingProgess'><i className="fa fa-spinner fa-spin fa-fw"></i> Sending</span>:'Send'}
+              </button>
+
               <div className='clearfix'></div>
             </div>
             <div className='panel-body'>
               <div style={style}>
 
-
                 <div className='form-group row'>
                   <label className='col-sm-2 control-label'>Account:</label>
                   <div className='col-sm-5'>
                       <label className="checkbox-inline">
-                        <input type="radio" label="GMAIL" checked={this.state.useGmail} />
+                        <input  type="radio"
+                                label="GMAIL"
+                                checked={this.state.useGmail}
+                                onClick={ ()=> {
+                                  this.setState({ useGmail: true, useSmtp: false });
+                                } }
+                                />
                         {' '} GMAIL
                       </label>
                       <label className="checkbox-inline">
-                        <input type="radio" label="SMTP" checked={this.state.useSmtp} />
+                        <input  type="radio"
+                                label="SMTP"
+                                checked={this.state.useSmtp}
+                                onClick={ ()=> {
+                                  this.setState({ useSmtp: true, useGmail: false });
+                                } }
+                                />
                          {' '} SMTP
                       </label>
                   </div>
@@ -71,78 +153,28 @@ class SendEmail extends React.Component {
                   </div>
                 </div>
                 <hr/>
-                <div className='form-group row'>
-                  <label className='col-sm-2 control-label'>Send To:</label>
-                  <div className='col-sm-5'>
-                    <input className='form-control' value={this.state.sendTo}/>
-                  </div>
-                  <div className='col-sm-5 helptext'>
-                    <p>Address to send to.</p>
-                  </div>
-                </div>
 
-                <div className='form-group row'>
-                  <label className='col-sm-2 control-label'>From:</label>
-                  <div className='col-sm-5'>
-                    <input className='form-control' value={this.state.sendTo}/>
-                  </div>
-                  <div className='col-sm-5 helptext'>
-                    <p>From</p>
-                  </div>
-                </div>
-
-
-                <div className='form-group row'>
-                  <label className='col-sm-2 control-label'>Reply To:</label>
-                  <div className='col-sm-5'>
-                    <input className='form-control' value={this.state.sendTo}/>
-                  </div>
-                  <div className='col-sm-5 helptext'>
-                    <p>Reply To</p>
-                  </div>
-                </div>
-
-                <div className='form-group row'>
-                  <label className='col-sm-2 control-label'>Subject:</label>
-                  <div className='col-sm-5'>
-                    <input className='form-control' value={this.state.sendTo}/>
-                  </div>
-                  <div className='col-sm-5 helptext'>
-                    <p>Subject</p>
-                  </div>
-                </div>
+                <EmailEntryItem label='Send To' value={this.state.sendTo} description='Address to send to.' onChange={(newValue)=>{ this.setState({ sendTo: newValue }); }} />
+                <EmailEntryItem label='From' value={this.state.from} description='From' onChange={(newValue)=>{ this.setState({ from: newValue }); }} />
+                <EmailEntryItem label='Reply To' value={this.state.replyTo} description='Reply To.' disabled={this.state.useGmail} onChange={(newValue)=>{ this.setState({ replyTo: newValue }); }} />
+                <EmailEntryItem label='Subject' value={this.state.subject} description='Subject.' onChange={(newValue)=>{ this.setState({ subject: newValue }); }} />
 
                 <hr />
 
-                <div className='form-group row'>
-                  <label className='col-sm-2 control-label'>Formats:</label>
+                <EmailFormatSelector  htmlFormats={this.props.outputs.filter((output)=>{ return output.type === 'html'; }).map((output)=>{return output.name;})}
+                                      textFormats={this.props.outputs.filter((output)=>{ return output.type === 'markdown'; }).map((output)=>{return output.name;})}
+                                      htmlEnabled={this.state.html}
+                                      textEnabled={this.state.text}
+                                      htmlSelection={this.state.htmlSelection}
+                                      textSelection={this.state.textSelection}
+                                      setHTML={(enabled, name)=>{
+                                        this.setState({ html: enabled, htmlSelection: (typeof name === undefined ? '': name) });
+                                      }}
+                                      setTEXT={(enabled, name)=>{
+                                        this.setState({ text: enabled, textSelection: (typeof name === undefined ? '': name) });
+                                      }}
+                                      />
 
-                  <div className='col-sm-5'>
-                    <div className='col-sm-3'><input  type='checkbox' checked={this.state.html} />{' '}HTML</div>
-                    <div className='col-sm-9 form-group'>
-                      <select className='form-control'>
-                        <option>something</option>
-                        <option>something</option>
-                        <option>something</option>
-                      </select>
-                    </div>
-
-                    <div className='col-sm-3'><input  type='checkbox' checked={this.state.html} />{' '}HTML</div>
-                    <div className='col-sm-9 form-group'>
-                      <select className='form-control'>
-                        <option>something</option>
-                        <option>something</option>
-                        <option>something</option>
-                      </select>
-                    </div>
-                  </div>
-
-                  <div className='col-sm-5'>
-                  What content to send
-                  </div>
-
-
-                </div> {/* end row */}
               </div>
 
         </div>
@@ -155,6 +187,7 @@ class SendEmail extends React.Component {
 
 SendEmail.propTypes = {
   config: React.PropTypes.object,
+  outputs: React.PropTypes.array,
   height: React.PropTypes.number
 };
 
--- src/Email/SendEmailContainer.js
+++ src/Email/SendEmailContainer.js
@@ -4,6 +4,7 @@ const SendEmail = require('./SendEmail.jsx');
 const mapStateToProps = (state) => {
   return {
     config: state.config.email,
+    outputs: state.output,
     height: state.uistate.height-50
   };
 };
